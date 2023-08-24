6dba949dfa31f9cbb4b46caafe05cdc2f05de313
addienaf <addienaf2001@gmail.com>
2023-08-24 20:19:34 +0700

    Update Css GGZ

--- resources/views/Component/sidebar.blade.php
+++ resources/views/Component/sidebar.blade.php
@@ -1,65 +1,66 @@
 <div class="wrapper">
-        <!-- Sidebar  -->
-        <nav id="sidebar">
-            <div class="sidebar-header">
-                <h3>Bootstrap Sidebar</h3>
-            </div>
+    <!-- Sidebar  -->
+    <nav id="sidebar">
+        <div class="sidebar-header">
+            <h3>Diskominfo Jabar</h3>
+        </div>
 
-            <ul class="list-unstyled components">
-                <p>Dummy Heading</p>
-                <li class="active">
-                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
-                    <ul class="collapse list-unstyled" id="homeSubmenu">
-                        <li>
-                            <a href="#">Home 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Home 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Home 3</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-                    <ul class="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                            <a href="#">Page 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 3</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Portfolio</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
+        <ul class="list-unstyled components">
+            <p>Dummy Heading</p>
+            <li class="active">
+                <a>Dashboard</a>
+                {{-- <ul class="collapse list-unstyled" id="homeSubmenu">
+                    <li>
+                        <a href="#">Home 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 3</a>
+                    </li>
+                </ul> --}}
+            </li>
+            <li>
+                <a href="{{ url('list') }}">Pendampingan</a>
+            </li>
+            <li>
+                <a href="{{ url('register') }}">Tambah User</a>
+                {{-- <a href="registerPage" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Tambah
+                    user</a> --}}
+                {{-- <ul class="collapse list-unstyled" id="pageSubmenu">
+                    <li>
+                        <a href="#">Page 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 3</a>
+                    </li>
+                </ul> --}}
+            </li>
+            {{-- <li>
+                <a href="#">Portfolio</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li> --}}
+        </ul>
+        <ul class="list-unstyled CTAs">
+            <li>
+                <a class="btn btn-primary" href="logout" role="button">Logout</a>
+            </li>
+            {{-- <li>
+                <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
+            </li> --}}
+        </ul>
+    </nav>
 
-            <ul class="list-unstyled CTAs">
-                <li>
-                    <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
-                </li>
-                <li>
-                    <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
-                </li>
-            </ul>
-        </nav>
+    <!-- Page Content  -->
+    <div id="content">
 
-        <!-- Page Content  -->
-        <div id="content">
-
-            {{-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        {{-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
 
                     <button type="button" id="sidebarCollapse" class="btn btn-info">
@@ -89,18 +90,20 @@
                 </div>
             </nav> --}}
 
-            
 
-            <h2><center>Icon orang</center>
-                <h1></h1></h2>
-           
-                {{-- <div class="container w-50 mt-5">
+
+        <h2>
+            <center>Icon orang</center>
+            <h1></h1>
+        </h2>
+
+        {{-- <div class="container w-50 mt-5">
                 <p><div class = "cardTable"></p> --}}
-                <p>
-                    <div class = "cardSidebar">
-                    <div class="container w-50 mt-5">
-                    <div class = "cardTable">
-                </p>
+        <p>
+        <div class="cardSidebar">
+            <div class="container w-50 mt-5">
+                <div div class="cardTable">
+
                 </div>
             </div>
-    </div>
+        </div>
